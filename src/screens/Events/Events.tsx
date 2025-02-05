68fba1a1bcca5fecfb093319e81903aeca427c59
Sethu Raman G <sethuraman.ganesan@numentica-ui.com>
2025-02-05 16:18:00 +0530

    SG - Quick fix

--- src/screens/Events/Events.tsx
+++ src/screens/Events/Events.tsx
@@ -15,53 +15,49 @@ import { CallToAction } from "../../components/ui/callToAction";
 
 export const Events = (): JSX.Element => {
   return (
-    <div className="">
-      <div className="w-full">
-        <Carousel>
-          <div className="flex justify-between items-center">
-            <div className="flex mb-8">
-              <h2 className="text-4xl font-medium text-BrandBlueHeavy">
-                {insightsTabData.eventsTabTitle}
-              </h2>
-            </div>
-            {insightsEventsData?.length > 3 ? (
+    <div>
+      <Carousel>
+        <div className="flex justify-between items-center">
+          <div className="flex mb-8">
+            <h2 className="text-4xl font-medium text-BrandBlueHeavy">
+              {insightsTabData.eventsTabTitle}
+            </h2>
+          </div>
+          {insightsEventsData?.length > 3 ? (
             <div className="flex mb-8">
               <CarouselPrevious />
               <CarouselNext />
             </div>
-            ) : null}
-          </div>
-          <div className="flex">
-            <CarouselContent
-              carouselClassName="pb-10"
-            >
-              {insightsEventsData.map((event, index) => (
-                <CarouselItem
-                  key={index}
-                  className="xs:basis-1/2 sm:basis-1/2 md:basis-1/2 lg:basis-1/3"
-                >
-                  <Card key={event.id} className="border-none shadow-md">
-                    <CardContent className="p-0">
-                      <img
-                        src={event.image}
-                        alt={event.title}
-                        className="w-full h-[341px] object-cover rounded-t-lg"
-                      />
-                      <div className="p-6 bg-white border-none rounded-b-lg">
-                        <h3 className="text-xl font-bold text-blue-900 mb-2">
-                          {event.title}
-                        </h3>
-                        <p className="text-gray-600">{event.description}</p>
-                      </div>
-                    </CardContent>
-                  </Card>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-          </div>
-        </Carousel>
-        <CallToAction />
-      </div>
+          ) : null}
+        </div>
+        <div className="flex">
+          <CarouselContent carouselClassName="pb-10">
+            {insightsEventsData.map((event, index) => (
+              <CarouselItem
+                key={index}
+                className="xs:basis-1/2 sm:basis-1/2 md:basis-1/2 lg:basis-1/3"
+              >
+                <Card key={event.id} className="border-none shadow-md">
+                  <CardContent className="p-0">
+                    <img
+                      src={event.image}
+                      alt={event.title}
+                      className="w-full h-[341px] object-cover"
+                    />
+                    <div className="p-6 bg-white border-none rounded-b-lg">
+                      <h3 className="text-xl font-bold text-blue-900 mb-2">
+                        {event.title}
+                      </h3>
+                      <p className="text-gray-600">{event.description}</p>
+                    </div>
+                  </CardContent>
+                </Card>
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        </div>
+      </Carousel>
+      <CallToAction />
     </div>
   );
 };
