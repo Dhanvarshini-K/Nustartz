1887ed4a1d31efd0e7390391aa0b1f8cce41a5fb
Sethu Raman G <sethuraman.ganesan@numentica-ui.com>
2025-02-18 16:27:21 +0530

    Case Study UI Changes

--- src/lib/caseStudyData.ts
+++ src/lib/caseStudyData.ts
@@ -1,126 +1,293 @@
-type CaseStudyDataType = {
-  title: string;
-  sections: {
-    heading: string;
-    content: string[];
+type CaseStudyType = {
+  title?: string;
+  sections?: {
+    heading?: string;
+    subHeading1?: string;
+    contents?: {
+      headingDescription?: string;
+      contentTitle?: string;
+      contentDescription?: string;
+      headingConclusion?: string;
+    }[];
+    subHeading2?: string;
+    subContents?: {
+      contentTitle?: string;
+      contentDescription?: string;
+    }[];
   }[];
 };
 
-export const caseStudyData1: CaseStudyDataType = {
+export const caseStudyData1: CaseStudyType = {
   title:
     "Revolutionizing Enterprise Search for Beyond Media Solutions with NuStartz",
   sections: [
     {
       heading: "Executive Summary",
-      content: [
-        "Beyond Media Solutions faced challenges with fragmented data retrieval across multiple platforms. To address this, the 'Enterprise Search Assistant' was developed—an advanced AI-powered search platform and chat assistant designed to unify search capabilities and improve user interaction. This solution greatly enhanced search efficiency and the overall user experience across diverse organizational data sources.",
+      contents: [
+        {
+          headingDescription:
+            "Beyond Media Solutions faced challenges with fragmented data retrieval across multiple platforms. To address this, the 'Enterprise Search Assistant' was developed—an advanced AI-powered search platform and chat assistant designed to unify search capabilities and improve user interaction. This solution greatly enhanced search efficiency and the overall user experience across diverse organizational data sources.",
+        },
       ],
     },
     {
       heading: "The Challenge",
-      content: [
-        "Fragmented Data Retrieval",
-        "Disparate Sources: Users struggled to retrieve relevant information from scattered websites and databases.",
-        "Search Efficiency: Finding key data quickly was difficult due to inadequate search tools.",
-        "User Experience: Ineffective user interfaces slowed productivity and lowered satisfaction.",
-        "Integration Issues: Seamless integration with internal tools was lacking.",
+      subHeading1: "Fragmented Data Retrieval",
+      contents: [
+        {
+          contentTitle: "Disparate Sources:",
+          contentDescription:
+            "Users struggled to retrieve relevant information from scattered websites and databases.",
+        },
+        {
+          contentTitle: "Search Efficiency:",
+          contentDescription:
+            "Finding key data quickly was difficult due to inadequate search tools.",
+        },
+        {
+          contentTitle: "User Experience:",
+          contentDescription:
+            "Ineffective user interfaces slowed productivity and lowered satisfaction.",
+        },
+        {
+          contentTitle: "Integration Issues:",
+          contentDescription:
+            "Seamless integration with internal tools was lacking.",
+        },
       ],
     },
     {
       heading: "The Solution",
-      content: [
-        "Integrated Search Platform",
-        "Enterprise Search Assistant: A unified, AI-powered search solution was created to enhance data search efficiency.",
-        "Enhanced User Interaction: A generative AI-powered chat assistant provided a conversational interface for natural language interaction, customizable for various contexts.",
+      subHeading1: "Integrated Search Platform",
+      contents: [
+        {
+          contentTitle: "Enterprise Search Assistant:",
+          contentDescription:
+            "A unified, AI-powered search solution was created to enhance data search efficiency.",
+        },
+        {
+          contentTitle: "Enhanced User Interaction:",
+          contentDescription:
+            "A generative AI-powered chat assistant provided a conversational interface for natural language interaction, customizable for various contexts.",
+        },
       ],
     },
     {
       heading: "Implementation",
-      content: [
-        "Frontend: Built using React.js for a dynamic UI, styled with Tailwind CSS.",
-        "State Management: Leveraged Redux for efficient state handling.",
-        "Backend: Python Flask endpoints managed data requests and interactions.",
-        "Process: Developed and tested frontend and backend components for robustness, with smooth deployment via cloud run services.",
+      contents: [
+        {
+          contentTitle: "Frontend:",
+          contentDescription:
+            "Built using React.js for a dynamic UI, styled with Tailwind CSS.",
+        },
+        {
+          contentTitle: "State Management:",
+          contentDescription: "Leveraged Redux for efficient state handling.",
+        },
+        {
+          contentTitle: "Backend:",
+          contentDescription:
+            "Python Flask endpoints managed data requests and interactions.",
+        },
+        {
+          contentTitle: "Process:",
+          contentDescription:
+            "Developed and tested frontend and backend components for robustness, with smooth deployment via cloud run services.",
+        },
       ],
     },
     {
       heading: "Results",
-      content: [
-        "Search Efficiency: Drastically improved search speed and accuracy.",
-        "User Experience: A more intuitive and user-friendly interface increased satisfaction.",
-        "Unified Access: Simplified data retrieval from multiple sources, streamlining workflows and improving productivity.",
+      contents: [
+        {
+          contentTitle: "Search Efficiency:",
+          contentDescription: "Drastically improved search speed and accuracy.",
+        },
+        {
+          contentTitle: "User Experience:",
+          contentDescription:
+            "A more intuitive and user-friendly interface increased satisfaction.",
+        },
+        {
+          contentTitle: "Unified Access:",
+          contentDescription:
+            "Simplified data retrieval from multiple sources, streamlining workflows and improving productivity.",
+        },
       ],
     },
     {
       heading: "Conclusion",
-      content: [
-        "The collaboration with Beyond Media Solutions led to a successful deployment of a state-of-the-art Enterprise Search Assistant, significantly improving search capabilities, user interaction, and productivity across the organization.",
+      contents: [
+        {
+          headingDescription:
+            "The collaboration with Beyond Media Solutions led to a successful deployment of a state-of-the-art Enterprise Search Assistant, significantly improving search capabilities, user interaction, and productivity across the organization.",
+        },
       ],
     },
   ],
 };
 
-export const caseStudyData2 = {
+export const caseStudyData2: CaseStudyType = {
   title: "CareerJourney - Bridging the Gap Between Aspiration and Achievement",
   sections: [
     {
       heading: "Executive Summary",
-      content: [
-        "CareerJourney needed a complete platform revamp to overcome the limitations of its legacy system, which hindered usability, real-time communication, and user satisfaction. Within a two-month timeframe, CareerJourney was transformed into a dynamic platform that integrates new plan management, chat, video calls, and meeting features. Additionally, it involved migrating from Angular version 11 to 17 and transitioning the backend from PHP to NestJS. This revamp provided a robust ecosystem for connecting users with mentors and streamlining task management and skill development.",
+      contents: [
+        {
+          headingDescription:
+            "CareerJourney needed a complete platform revamp to overcome the limitations of its legacy system, which hindered usability, real-time communication, and user satisfaction. Within a two-month timeframe, CareerJourney was transformed into a dynamic platform that integrates new plan management, chat, video calls, and meeting features. Additionally, it involved migrating from Angular version 11 to 17 and transitioning the backend from PHP to NestJS. This revamp provided a robust ecosystem for connecting users with mentors and streamlining task management and skill development.",
+        },
       ],
     },
     {
       heading: "The Challenge",
-      content: [
-        "Legacy Platform Limitations:",
-        "User Experience: The outdated interface and limited features created usability issues.",
-        "Real-time Communication: There were no chat or video call services in place.",
-        "Complex Architecture: The system for managing content feeds was overly complicated.",
-        "User Satisfaction: Mentors and mentees required a simplified task-tracking system that had not been previously implemented.",
-        "Specific Challenges:",
-        "Tight two-month deadline to revamp the platform and implement new features.",
-        "Angular version migration from 11 to 17.",
-        "Migration of existing PHP code to a NestJS backend.",
+      subHeading1: "Legacy Platform Limitations:",
+      contents: [
+        {
+          contentTitle: "User Experience:",
+          contentDescription:
+            "The outdated interface and limited features created usability issues.",
+        },
+        {
+          contentTitle: "Real-time Communication:",
+          contentDescription:
+            "There were no chat or video call services in place.",
+        },
+        {
+          contentTitle: "Complex Architecture:",
+          contentDescription:
+            "The system for managing content feeds was overly complicated.",
+        },
+        {
+          contentTitle: "User Satisfaction:",
+          contentDescription:
+            "Mentors and mentees required a simplified task-tracking system that had not been previously implemented.",
+        },
+      ],
+      subHeading2: "Specific Challenges:",
+      subContents: [
+        {
+          contentDescription:
+            "Tight two-month deadline to revamp the platform and implement new features.",
+        },
+        { contentDescription: "Angular version migration from 11 to 17." },
+        {
+          contentDescription:
+            "Migration of existing PHP code to a NestJS backend.",
+        },
       ],
     },
     {
       heading: "The Solution",
-      content: [
-        "CareerJourney emerged as a comprehensive platform that addressed these challenges, combining a robust social network with advanced technology to create an ecosystem where users could connect, learn, and grow. The platform was successfully developed and migrated within the two-month timeline, implementing new features and a complete backend transition.",
+      contents: [
+        {
+          headingDescription:
+            "CareerJourney emerged as a comprehensive platform that addressed these challenges, combining a robust social network with advanced technology to create an ecosystem where users could connect, learn, and grow. The platform was successfully developed and migrated within the two-month timeline, implementing new features and a complete backend transition.",
+        },
       ],
     },
     {
       heading: "Key Features",
-      content: [
-        "Personalized Feed: Curated content showcasing relevant posts from followed users and mentors.",
-        "Mentorship and Guidance: A structured program that enables plan creation, task assignment, progress tracking, and one-on-one interactions.",
-        "Real-time Communication: Integrated chat and video call functionalities for immediate communication between users.",
-        "Skill Development: Access to resources, workshops, and courses for personal growth.",
-        "Job Placement Assistance: Integrated job boards to help users find jobs and internships.",
+      contents: [
+        {
+          contentTitle: "Personalized Feed:",
+          contentDescription:
+            "Curated content showcasing relevant posts from followed users and mentors.",
+        },
+        {
+          contentTitle: "Mentorship and Guidance:",
+          contentDescription:
+            "A structured program that enables plan creation, task assignment, progress tracking, and one-on-one interactions.",
+        },
+        {
+          contentTitle: "Real-time Communication:",
+          contentDescription:
+            "Integrated chat and video call functionalities for immediate communication between users.",
+        },
+        {
+          contentTitle: "Skill Development:",
+          contentDescription:
+            "Access to resources, workshops, and courses for personal growth.",
+        },
+        {
+          contentTitle: "Job Placement Assistance:",
+          contentDescription:
+            "Integrated job boards to help users find jobs and internships.",
+        },
       ],
     },
     {
       heading: "Technology Stack",
-      content: [
-        "Frontend: Angular with Angular Material for a responsive and modern interface.",
-        "Backend: NestJS for scalable API development.",
-        "Database: PostgreSQL for efficient data management.",
-        "Real-time Communication: Azure Communication Services for chat and video calls.",
-        "DevOps: Azure for cloud infrastructure and deployment.",
-        "Architecture: NX monorepo for streamlined development management.",
-        "Mobile App: Flutter for cross-platform mobile development.",
+      contents: [
+        {
+          contentTitle: "Frontend:",
+          contentDescription:
+            "Angular with Angular Material for a responsive and modern interface.",
+        },
+        {
+          contentTitle: "Backend:",
+          contentDescription: "NestJS for scalable API development.",
+        },
+        {
+          contentTitle: "Database:",
+          contentDescription: "PostgreSQL for efficient data management.",
+        },
+        {
+          contentTitle: "Real-time Communication:",
+          contentDescription:
+            "Azure Communication Services for chat and video calls.",
+        },
+        {
+          contentTitle: "DevOps:",
+          contentDescription: "Azure for cloud infrastructure and deployment.",
+        },
+        {
+          contentTitle: "Architecture:",
+          contentDescription:
+            "NX monorepo for streamlined development management.",
+        },
+        {
+          contentTitle: "Mobile App:",
+          contentDescription: "Flutter for cross-platform mobile development.",
+        },
       ],
     },
     {
       heading: "Impact and Results",
-      content: [
-        "CareerJourney successfully addressed the challenges faced by students and professionals, achieving the following:",
-        "Thriving Community: Fostered collaboration and belonging among users.",
-        "Empowered Individuals: Provided tools and support for career advancement.",
-        "Improved Career Outcomes: Helped users secure jobs, internships, and opportunities.",
-        "Increased Engagement: Higher user retention and engagement rates, signaling strong product-market fit.",
-        "Data-driven Insights: Leveraged platform data to continuously enhance user experience.",
-        "By merging user-centric design with cutting-edge technology, CareerJourney has become a preferred platform for individuals seeking to accelerate their career growth."
+      contents: [
+        {
+          headingDescription:
+            "CareerJourney successfully addressed the challenges faced by students and professionals, achieving the following:",
+        },
+        {
+          contentTitle: "Thriving Community:",
+          contentDescription:
+            " Fostered collaboration and belonging among users.",
+        },
+        {
+          contentTitle: "Empowered Individuals:",
+          contentDescription:
+            " Provided tools and support for career advancement.",
+        },
+        {
+          contentTitle: "Improved Career Outcomes:",
+          contentDescription:
+            " Helped users secure jobs, internships, and opportunities.",
+        },
+        {
+          contentTitle: "Increased Engagement:",
+          contentDescription:
+            " Higher user retention and engagement rates, signaling strong product-market fit.",
+        },
+        {
+          contentTitle: "Data-driven Insights:",
+          contentDescription:
+            " Leveraged platform data to continuously enhance user experience.",
+        },
+        {
+          headingConclusion:
+            "By merging user-centric design with cutting-edge technology, CareerJourney has become a preferred platform for individuals seeking to accelerate their career growth.",
+        },
       ],
     },
   ],
--- src/screens/CaseStudy/CaseStudy.tsx
+++ src/screens/CaseStudy/CaseStudy.tsx
@@ -2,7 +2,7 @@ import { useLocation } from "react-router-dom";
 import { caseStudyData1, caseStudyData2 } from "../../lib/caseStudyData";
 
 export const CaseStudy = () => {
-  const {state} = useLocation();
+  const { state } = useLocation();
 
   const caseStudyData = state === "01" ? caseStudyData1 : caseStudyData2;
   return (
@@ -11,20 +11,65 @@ export const CaseStudy = () => {
         <h1 className="text-3xl sm:text-6xl font-bold mb-5 sm:mb-10">
           {caseStudyData.title}
         </h1>
-        {caseStudyData.sections.map((section, index) => (
+        {caseStudyData?.sections?.map((section, index) => (
           <div key={index}>
-            <h2 className="text-2xl text-BrandPurple font-semibold my-4">
+            <h2 className="text-2xl text-BrandPurple font-semibold my-2">
               {section.heading}
             </h2>
-            {section.content.length > 1 ? (
-            <ul className="text-lg sm:text-xl text-ParaGrey">
-              {section.content.map((point, index) => (
-                <li key={index}>- {point}</li>
-              ))}
-            </ul>
-          ) : (
-            <p className="text-lg sm:text-xl text-ParaGrey">{section.content[0]}</p>
-          )}
+            {section.contents?.length === 1 ? (
+              <p className="text-lg sm:text-xl text-ParaGrey">
+                {section?.contents?.[0]?.headingDescription}
+              </p>
+            ) : (
+              <div>
+                <p className="text-lg font-semibold my-1">
+                  {section.subHeading1}
+                </p>
+
+                {section?.contents?.map((content, index) => (
+                  <div key={index}>
+                    {content.headingDescription && (
+                      <p className="text-lg sm:text-xl text-ParaGrey mb-1">
+                        {content.headingDescription}
+                      </p>
+                    )}
+                    {(content?.contentTitle || content?.contentDescription) && (
+                      <ul className="text-md sm:text-xl text-ParaGrey">
+                        <li className="ml-5 list-disc list-inside text-md sm:text-lg">
+                          <span className="font-semibold text-md sm:text-lg">
+                            {content?.contentTitle}
+                          </span>{" "}
+                          {content?.contentDescription}
+                        </li>
+                      </ul>
+                    )}
+
+                    {content.headingConclusion && (
+                      <p className="text-lg sm:text-xl text-ParaGrey mb-1">
+                        {content.headingConclusion}
+                      </p>
+                    )}
+                  </div>
+                ))}
+
+                <p className="text-lg font-semibold my-1">
+                  {section.subHeading2}
+                </p>
+
+                {section?.subContents?.length ? (
+                  <ul className="text-lg sm:text-xl text-ParaGrey">
+                    {section.subContents.map((subContent, index) => (
+                      <li
+                        key={index}
+                        className="ml-5 list-disc list-inside text-lg"
+                      >
+                        {subContent.contentDescription}
+                      </li>
+                    ))}
+                  </ul>
+                ) : null}
+              </div>
+            )}
           </div>
         ))}
       </div>
