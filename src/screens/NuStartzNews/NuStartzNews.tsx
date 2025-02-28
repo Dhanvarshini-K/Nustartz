09dd2552d884c3c8d1f44a6be331abdb4454fd02
Sethu Raman G <sethuraman.ganesan@numentica-ui.com>
2025-02-28 21:59:32 +0530

    Insights news article changes

--- src/screens/NuStartzNews/NuStartzNews.tsx
+++ src/screens/NuStartzNews/NuStartzNews.tsx
@@ -1,12 +1,12 @@
 import { useLocation } from "react-router-dom";
-import { nuStartzNewsData } from "../../lib/nustartzNewsData";
+import { nustartzNewsContents } from "../../lib/nustartzNewsData";
 import { NotFound } from "../../components/ui/notFound";
 
 export const NuStartzNews = () => {
   const { state } = useLocation();
   const { image } = state || {};
 
-  const nustartzNewsArticleData = nuStartzNewsData.find(
+  const nustartzNewsArticleData = nustartzNewsContents.find(
     (news) => news.image1 === image
   );
 
@@ -14,46 +14,72 @@ export const NuStartzNews = () => {
     <section className="bg-custom-gradient px-4 sm:px-20">
       {nustartzNewsArticleData !== undefined ? (
         <div className="section-container py-10 sm:py-16">
-          <h1 className="text-3xl sm:text-6xl font-bold mb-5 sm:mb-10">
+          <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold mb-0 sm:mb-5">
             {nustartzNewsArticleData?.title}
           </h1>
 
-          <p className="text-xl sm:text-3xl text-ParaGrey ">
-            {nustartzNewsArticleData?.subtitle}
+          <p className="text-xl sm:text-2xl text-ParaGrey ">
+            {nustartzNewsArticleData?.subTitle}
           </p>
 
           <div className="flex gap-10 py-10 flex-col-reverse lg:flex-row justify-between">
             <div className="w-full lg:w-[75%] flex flex-col justify-center">
-              <p className="text-lg sm:text-xl text-ParaGrey pb-4">
-                {nustartzNewsArticleData?.imageDescription1}
-              </p>
-              <p className="text-lg sm:text-xl text-ParaGrey">
-                {nustartzNewsArticleData?.imageDescription2}
-              </p>
+              {nustartzNewsArticleData?.imageDescriptions1?.map(
+                (description, index) => (
+                  <p
+                    className="text-lg sm:text-xl text-ParaGrey py-2"
+                    key={index}
+                  >
+                    {description}
+                  </p>
+                )
+              )}
             </div>
             <div className="flex items-center justify-center">
               <img
                 src={nustartzNewsArticleData?.image1}
                 alt="NustartzNews"
-                className="w-[300px] sm:w-[350px]"
+                className="w-[300px]"
               />
             </div>
           </div>
 
-          <div className="flex gap-10 flex-col lg:flex-row justify-between">
+          <div className="flex gap-10 flex-col lg:flex-row justify-between pb-10">
             <div className="w-full flex items-center justify-center">
               <img
                 src={nustartzNewsArticleData?.image2}
                 alt="NustartzArticle"
-                className="w-[300px] sm:w-[350px] rounded-xl"
+                className="w-[300px] rounded-xl"
               />
             </div>
-            <div className="flex items-center">
-              <p className="text-lg sm:text-xl text-ParaGrey w-full">
-                {nustartzNewsArticleData?.description2}
-              </p>
+            <div className="flex flex-col items-center justify-center gap-5">
+              {nustartzNewsArticleData?.imageDescriptions2?.map(
+                (description, index) => (
+                  <p
+                    className="text-lg sm:text-xl text-ParaGrey w-full"
+                    key={index}
+                  >
+                    {description}
+                  </p>
+                )
+              )}
             </div>
           </div>
+
+          {nustartzNewsArticleData?.news?.map((news, index) => (
+            <div key={index}>
+              <h2 className="text-2xl text-BrandPurple font-bold">
+                {news.newsTitle}
+              </h2>
+              <div className="ml-6 p-3 sm:p-5">
+                {news.newsDescriptions?.map((keyPoint, index) => (
+                  <p className="text-lg text-ParaGrey pb-2" key={index}>
+                    {keyPoint}
+                  </p>
+                ))}
+              </div>
+            </div>
+          ))}
         </div>
       ) : (
         <NotFound />
